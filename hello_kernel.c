d8e3e233989d870e1e99c45fe75bd305a98c83b0
DanielFeldman1 <daniel.feldman303@gmail.com>
2026-03-20 16:16:41 +0200

    refactor: swap printk to pr_info

--- hello_kernel.c
+++ hello_kernel.c
@@ -8,12 +8,12 @@ MODULE_AUTHOR("TheBinaryBjorn");
 MODULE_DESCRIPTION("A simple first Linux kernel module");
 
 static int __init hello_init(void) {
-    printk(KERN_INFO "Jarvis Kernel: Module loaded. Hello from Kernel Space!\n");
+    pr_info("Kernel: Module loaded. Hello from Kernel Space!\n");
     return 0;
 }
 
 static void __exit hello_exit(void) {
-    printk(KERN_INFO "Jarvis Kernel: Module unloaded. Goodbye!\n");
+    pr_info("Kernel: Module unloaded. Goodbye!\n");
 }
 
 module_init(hello_init);
