aa97d1331ac431622a0193452097e5ae950052fe
DanielFeldman1 <daniel.feldman303@gmail.com>
2026-03-20 14:53:52 +0200

    feat: add basic linux kernel module skeleton

---
+++ hello_kernel.c
@@ -0,0 +1,20 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+// Metadata
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("TheBinaryBjorn");
+MODULE_DESCRIPTION("A simple first Linux kernel module");
+
+static int __init hello_init(void) {
+    printk(KERN_INFO, "Jarvis Kernel: Module loaded. Hello from Kernel Space!\n");
+    return 0;
+}
+
+static void __exit hello_exit(void) {
+    printk(KERN_INFO, "Jarvis Kernel: Module unloaded. Goodbye!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
